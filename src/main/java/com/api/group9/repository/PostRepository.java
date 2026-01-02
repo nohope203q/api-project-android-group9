6715dc9fef765c7024dbac19f600776ad3dc6b1d
Huỳnh Thiên Hạo <hq301119@gmail.com>
2026-01-02 23:52:00 +0700

    update speeed

--- src/main/java/com/api/group9/repository/PostRepository.java
+++ src/main/java/com/api/group9/repository/PostRepository.java
@@ -10,6 +10,11 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query("SELECT p FROM Post p WHERE p.userId IN :userIds ORDER BY p.createdAt DESC")
-    Page<Post> findNewsFeed(@Param("userIds") List<Long> userIds, Pageable pageable);
+
+    @Query(value = "SELECT p FROM Post p " +
+        "LEFT JOIN FETCH p.user " +  // Chỉ cần Fetch User thôi
+        "WHERE p.userId IN :userIds " +
+        "ORDER BY p.createdAt DESC",
+        countQuery = "SELECT COUNT(p) FROM Post p WHERE p.userId IN :userIds")
+Page<Post> findNewsFeed(@Param("userIds") List<Long> userIds, Pageable pageable);
 }
